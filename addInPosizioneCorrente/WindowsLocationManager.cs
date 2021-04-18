d3fb502ea9fe05e871317cd44925b5e787db62f1
Giancarlo Messina <giancarlomex@outlook.com>
2021-04-18 17:59:43 +0200

    Add project files.

---
+++ addInPosizioneCorrente/WindowsLocationManager.cs
@@ -0,0 +1,61 @@
+﻿using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geometry;
+using System;
+using System.Device.Location;
+using System.Windows.Forms;
+
+namespace addInPosizioneCorrente
+{
+    public class PosizioneAgganciataEventArgs : EventArgs
+    {
+        public IPoint Punto { get; set; }
+    }
+    public class WindowsLocationManager
+    {
+        public event EventHandler<PosizioneAgganciataEventArgs> PosizioneAgganciataEventHandler;
+        private readonly IMxDocument _mxDocument = (IMxDocument)ArcMap.Application.Document;
+        private readonly IMap _map = null;
+        private readonly ISpatialReferenceFactory3 _srFactory = null;
+        private readonly ISpatialReference3 _SRMappa = null;
+        private readonly ISpatialReference3 _SR_WGS84 = null;
+        private const int _EPSG_WGS84 = 4326;
+        private readonly int _EPSG_MAPPA = -9999;
+
+        public WindowsLocationManager()
+        {
+            _map = _mxDocument.FocusMap;
+            _srFactory = new SpatialReferenceEnvironment() as ISpatialReferenceFactory3;
+            _EPSG_MAPPA = _map.SpatialReference.FactoryCode;
+            _SR_WGS84 = (ISpatialReference3)_srFactory.CreateSpatialReference(_EPSG_WGS84);
+            _SRMappa = (ISpatialReference3)_srFactory.CreateSpatialReference(_EPSG_MAPPA);
+            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
+            watcher.PositionChanged += Watcher_PositionChanged;
+            bool bStarted = watcher.TryStart(false, TimeSpan.FromMilliseconds(2000));
+            if (!bStarted) MessageBox.Show("GeoCoordinateWatcher timed out all'avvio", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void Watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            GeoCoordinate coordinateDiWindows = e.Position.Location;
+
+            IPoint punto = new PointClass()
+            {
+                X = coordinateDiWindows.Longitude,
+                Y = coordinateDiWindows.Latitude,
+                SpatialReference = _SR_WGS84
+            };
+
+            // Proietto il punto
+            if (_EPSG_MAPPA != _EPSG_WGS84) punto.Project(_SRMappa);
+
+            PosizioneAgganciataEventArgs posizioneAgganciataEventArgs = new PosizioneAgganciataEventArgs
+            {
+                Punto = punto
+            };
+
+            PosizioneAgganciataEventHandler?.Invoke(this, posizioneAgganciataEventArgs);
+        }
+    }
+}
---
+++ addInPosizioneCorrente/btnPosizione.cs
@@ -0,0 +1,61 @@
+﻿using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Geometry;
+using GiancaGISLibrary;
+using System;
+using System.Windows.Forms;
+
+namespace addInPosizioneCorrente
+{
+    public class btnPosizione : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        private readonly IMxDocument _mxDocument = (IMxDocument)ArcMap.Application.Document;
+        private readonly IMap _map = null;
+        private readonly IActiveView _activeView = null;
+        private IPoint _punto = null;
+
+
+        public btnPosizione()
+        {
+            _mxDocument = (IMxDocument)ArcMap.Application.Document;
+            _map = _mxDocument.FocusMap;
+            _activeView = _mxDocument.ActiveView;
+            WindowsLocationManager windowsLocationManager = new WindowsLocationManager();
+            windowsLocationManager.PosizioneAgganciataEventHandler += WindowsLocationManager_PosizioneAgganciataEventHandler;
+        }
+
+        protected override void OnClick()
+        {
+            if (_punto == null)
+                MessageBox.Show("Posizione non disponibile!\nVerificare i permessi utente", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else
+            {
+                IEnvelope currentEnv = _activeView.Extent;
+                currentEnv.CenterAt(_punto);
+                _activeView.Extent = currentEnv;
+                _map.MapScale = 5000;
+                IRgbColor spazioColoreRGB = new RgbColor();
+                Random random = new Random();
+                spazioColoreRGB.Green = random.Next(0, 255);
+                spazioColoreRGB.Blue = random.Next(0, 255);
+                spazioColoreRGB.Red = random.Next(0, 255);
+                
+                GiancaGISLibrary.ActiveView activeViewGiancaGIS = new GiancaGISLibrary.ActiveView();
+                activeViewGiancaGIS.AggiungiGraphicInMappa(_map, _punto, spazioColoreRGB, spazioColoreRGB, true, esriSimpleMarkerStyle.esriSMSDiamond);
+                _activeView.Refresh();
+            }
+        }
+
+        private void WindowsLocationManager_PosizioneAgganciataEventHandler(object sender, PosizioneAgganciataEventArgs e)
+        {
+            _punto = e.Punto;
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
